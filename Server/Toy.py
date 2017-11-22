1b5d0a04d4ad2d19ab480ded9d391cc6cbcb37f9
Hsieh Li-Cheng <tappy820918@gmail.com>
2017-11-22 15:06:25 +0800

    Add files via upload

--- Server/Toy.py
+++ Server/Toy.py
@@ -1,4 +1,4 @@
-__version__ ='0.0.1'
+__version__ ='0.0.3'
 import time
 from datetime import datetime
 
@@ -7,4 +7,7 @@ if __name__ == '__main__':
     Time = datetime.now()
     while(1):
         print "[ Toy version",__version__,']\t @', Time
-        time.sleep(1)
+        print 'This is Claude!'
+        print "\n"
+        print "\n"
+        time.sleep(10)
