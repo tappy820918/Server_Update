458f1cd08033742c25434930e5341e4ea826e553
Hsieh Li-Cheng <tappy820918@gmail.com>
2017-11-22 15:05:53 +0800

    Add files via upload

--- Update.py
+++ Update.py
@@ -8,30 +8,39 @@ from subprocess import check_output
 from urllib import urlopen
 
 ############# Initial Setting ############
-#MAC = '10.137.98.199'                   #
-MAC = '192.168.0.107'                    #
-csv_path = 'config.csv'                  #
-main_file = 'Toy.py'                     #
+MAC = '10.137.99.200'                    #
+#MAC = '192.168.0.110'                   #
+__configuration__ = 'config.csv'         #
+__mainfile__ = 'Toy.py'                  #
+__setting__ = 'Toy_setting.csv'          # 
 __version__ = '0.0.1'                    #
 ##########################################
 
-csv_get_url = 'http://'+MAC+':8000/Server/'+csv_path
-main_file_get_url = 'http://'+MAC+':8000/Server/'+main_file
-execute_cmd = 'python ' + main_file
+csv_get_url = 'http://'+MAC+':8000/Server/'+__configuration__
+main_file_get_url = 'http://'+MAC+':8000/Server/'+__mainfile__
+setting_get_url = 'http://'+MAC+':8000/Server/'+__setting__
+
+execute_cmd = 'python ' + __mainfile__
 execute_cmd_background = execute_cmd + ' &'
-check_output_cmd = 'python -c "from '+main_file[:-3]+' import __version__; print __version__"'
+check_output_cmd = 'python -c "from '+__mainfile__[:-3]+' import __version__; print __version__"'
 PIDKILL = ''
-__version__ = check_output(check_output_cmd,shell = True)
+__version__ = '0.0.0'
 def client_get_csv():
-    print "Downloading file " + csv_path + "..."
+    print "Downloading file " + __configuration__ + "..."
     r = urlopen(csv_get_url, proxies=None)
-    with open(csv_path, 'wb') as fd:
+    with open(__configuration__, 'wb') as fd:
         fd.write(r.read())
 
 def client_get_main_file():
-    print "Downloading file " + main_file + "..."
+    print "Downloading file " + __mainfile__ + "..."
     r = urlopen(main_file_get_url, proxies=None)
-    with open(main_file, 'wb') as fd:
+    with open(__mainfile__, 'wb') as fd:
+        fd.write(r.read())
+
+def client_get_setting():
+    print "Downloading file " + __setting__ + "..."
+    r = urlopen(setting_get_url, proxies=None)
+    with open(__setting__, 'wb') as fd:
         fd.write(r.read())
 
 def restart_background_program():
@@ -43,7 +52,15 @@ def find_program(program_list):
             return list
 
 def get_pid():
-    fetch_pid = 'ps -ef |grep '+ main_file
+    '''
+        In this function we want to get the pid of the executing command
+        (the number"5724" in the bellow example)
+        
+        ubuntu@1605I0000033:$ ps -ef |grep Toy.py
+        ubuntu     5724  6364  0 09:08 pts/4    00:00:00 python Toy.py
+        ubuntu     5725  6347  0 09:09 pts/3    00:00:00 grep --color=auto Toy.py
+    '''
+    fetch_pid = 'ps -ef |grep '+ __mainfile__
     program_list = check_output(fetch_pid, shell=True).split("\n")
     for list in program_list:
         if list[-len(execute_cmd):] == execute_cmd:
@@ -51,42 +68,61 @@ def get_pid():
     pID = [x for x in pid_list if x][1]
     KILL = 'kill '+ pID
     return KILL
-    print "Terminating program ",main_file,"-[",pID,"]"
+    print "Terminating program ",__mainfile__,"-[",pID,"]"
 
 def read_config_csv():
-    f = open(csv_path, "r")
+    f = open(__configuration__, "r")
     data = [x.strip() for x in f.readlines()]
     f.close()
     return (data)
 
 def write_config_csv(data):
-    f = open(csv_path, "w")
+    f = open(__configuration__, "w")
     for string in data:
         f.writelines(string + '\n')
     f.close()
 
+def read_setting_csv():
+    f = open(__setting__, "r")
+    data = [x.strip() for x in f.readlines()]
+    f.close()
+    return (data)
+
 if __name__ == '__main__':
     client_get_csv()
+    client_get_setting()
     client_get_main_file()
     restart_background_program()
     PIDKILL = get_pid()
     __version__ = check_output(check_output_cmd,shell = True)
-    print "Current version is ", __version__
+    print "Current version is", __version__
+    print "\n"
     while(1):
         try:
             client_get_csv()
             Update_list = read_config_csv()
+            Setting_list = read_setting_csv()
+            print "Setting_list",Setting_list
             print "Update_list: ",Update_list, "Current running version :",__version__
             if StrictVersion(Update_list[0]) > StrictVersion(__version__):
                 os.system(PIDKILL)
                 client_get_main_file()
                 __version__ = check_output(check_output_cmd,shell = True)
-                print "<===== RESTART ",main_file," version ", __version__[:-1]," =====>"
+                print "<===== RESTART ",__mainfile__," version ", __version__[:-1]," =====>"
+                print "\n"
                 restart_background_program()
                 PIDKILL = get_pid()
-                
-            time.sleep(5)
+
+            if StrictVersion(Update_list[1]) > StrictVersion(Setting_list[0]):
+                client_get_setting()
+                print "<===== RESTART ",__setting__," version ", Setting_list[0]," =====>"
+                print "\n"
+            
+            time.sleep(25)
         except KeyboardInterrupt:
             os.system(PIDKILL)
-            print "\n KeyboardInterrupt => Kill",main_file,"--pid ",PIDKILL[6:]
+            print "\n KeyboardInterrupt => Kill",__mainfile__,"--pid ",PIDKILL[6:]
+            break
+        except:
+            print "Error. Terminating Upating system"
             break
